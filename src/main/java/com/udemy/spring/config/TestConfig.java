b37a6692b3cef944f6e3e0208bae0bcf4bd36308
JandeAJR <jande.ajr@gmail.com>
2025-12-28 15:42:16 -0300

    Commig class TestConfig

--- src/main/java/com/udemy/spring/config/TestConfig.java
+++ src/main/java/com/udemy/spring/config/TestConfig.java
@@ -5,6 +5,7 @@ import java.time.ZoneId;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -17,7 +18,7 @@ import com.udemy.spring.infrastructure.repositories.RequestRepository;
 
 @Configuration
 @Profile("test")
-public class TestConfig {
+public class TestConfig implements CommandLineRunner {
 	@Autowired
     private CustomerRepository customerRepository;
     @Autowired
