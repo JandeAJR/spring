cfbfe68ff769e83d5e9fca2c823e68cc4b3a09dc
JandeAJR <jande.ajr@gmail.com>
2025-12-31 18:29:00 -0300

    Add OpenAPI documentation and migrate application properties to YAML format

--- src/main/java/com/udemy/spring/Application.java
+++ src/main/java/com/udemy/spring/Application.java
@@ -4,8 +4,44 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@EnableFeignClients // Habilita o uso do OpenFeign para comunicação entre serviços (apis externas)
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Spring Pizza API 🍕",
+        version = "v1",
+        description = """
+            API responsável pelo gerenciamento de pedidos, clientes e pizzas.
+            Desenvolvida seguindo boas práticas REST, princípios de domínio
+            e foco em clareza, previsibilidade e escalabilidade.
+            """,
+        contact = @Contact(
+            name = "Time de Arquitetura",
+            email = "arquitetura@empresa.com",
+            url = "https://empresa.com"
+        ),
+        license = @License(
+            name = "Apache 2.0",
+            url = "https://www.apache.org/licenses/LICENSE-2.0"
+        )
+    ),
+    servers = {
+        @Server(
+            url = "http://localhost:8080/api/spring",
+            description = "Ambiente Local"
+        ),
+        @Server(
+            url = "https://api.empresa.com/api/spring",
+            description = "Produção"
+        )
+    }
+)
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
